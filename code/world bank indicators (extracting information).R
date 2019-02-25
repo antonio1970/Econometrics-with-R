b17ae1ceba88f0d221188c5cdbc5244410c0d29e
antonio1970 <antoniorodriguezandres70@gmail.com>
2019-02-25 15:28:10 +0100

    'code'

--- code/world bank indicators (extracting information).R
+++ code/world bank indicators (extracting information).R
@@ -2,14 +2,7 @@
 
 library(wbstats)
 
-# Some indicators, population, Trade (%GDP)
+# Some indicators, population, Trade (%GDP), option return wide = TRUE
 panel_data<- wb(indicator = c("SP.POP.TOTL", "SP.POP.GROW", "NE.TRD.GNFS.ZS"), startdate = 1960, enddate = 2017, return_wide = TRUE)
 head(panel_data)
 
-#Rearrange panel in wide format (variables per column)
-
-panel <-panel_data %>% 
-  arrange(country, date) 
-
-  
-head(panel, 5)
